dd15c89bd687f9de3b000b3e7738cbc68b94d068
evcapp <157657958+evcapp@users.noreply.github.com>
2024-02-09 00:19:12 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,31 @@
 import random
-num = random.randint(1, 100)
 
-user = input("Enter your guess: ")
-user = int(user)
+while True:
+    num = random.randint(1, 100)
+    guess_count = 0
+    lower_bound = 1
+    upper_bound = 100
+    
+    while True:
+        print(f"Guess a number between {lower_bound} and {upper_bound}:")
+        user = int(input())
+        guess_count += 1
+        
+        if user > num:
+            print("Too high!")
+            upper_bound = user
+        elif user < num:
+            print("Too low!")
+            lower_bound = user
+        else:
+            print("You win!")
+            break
+    
+    print("It took you", guess_count, "guesses to solve it!")
+    
+    replay = input("Do you want to play again? (yes/no): ")
+    if replay.lower() != 'yes':
+        print("Goodbye!")
+        break
+ 
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
